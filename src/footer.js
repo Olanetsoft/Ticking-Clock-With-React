5641a3a907f5e91731bdfbba7b9d0cfab3f36449
olanetsoft <heedris2olubisi@gmail.com>
2020-01-19 16:30:04 +0100

    FOOTR.CSS CREATED AND IMPORTED IN FOOTER.JS

--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>View on Github: <a href="https://github.com/Olanetsoft/Ticking-Clock-With-React">Click Here</a></h2>
+        <div className="footer">
+            <h2>View Code: <a href="https://github.com/Olanetsoft/Ticking-Clock-With-React">Click Here</a></h2>
         </div>
     );
 };
