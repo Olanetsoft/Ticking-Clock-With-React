065921fb040ee5ceb698ba14e9927b4584fda43c
olanetsoft <heedris2olubisi@gmail.com>
2020-01-19 16:32:23 +0100

    TARGET INCLUDED

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <h2>View Code: <a href="https://github.com/Olanetsoft/Ticking-Clock-With-React">Click Here</a></h2>
+            <h2>View Code: <a href="https://github.com/Olanetsoft/Ticking-Clock-With-React" target="_blank">Click Here</a></h2>
         </div>
     );
 };
